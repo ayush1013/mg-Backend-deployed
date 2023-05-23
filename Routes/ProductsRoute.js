9cd57a62bbc16cec8f936bc798eb9fc646869baa
Ayush Verma <ayush98277@gmail.com>
2023-05-23 16:39:32 +0530

    sorting filtering updated

--- Routes/ProductsRoute.js
+++ Routes/ProductsRoute.js
@@ -1,24 +1,72 @@
-const express = require('express');
+const express = require("express");
 const productsRoute = express.Router();
-const ProductsModel = require('../Models/ProductsModel');
+const ProductsModel = require("../Models/ProductsModel");
 
-productsRoute.get("/", async(req,res)=>{
-    const find = req.query.find;
-    const {limit=20, page=1} = req.query;
-    // res.send("Welcome to the products page")
-    try{
-        if(find){
-            let products = await ProductsModel.find({
-                title: {$regex: find, $options: "i"}
-            }).limit(limit).skip(limit*(page-1));
-            res.send(products)
-        }else{
-            let products = await ProductsModel.find().limit(limit).skip(limit*(page-1));
-            res.send(products);
-        }
-    }catch(err){
-        res.send(err.message);
+// productsRoute.get("/", async (req, res) => {
+//   const text = req.query.text;
+//   const { limit = 20, page = 1, sort, order } = req.query;
+//   // res.send("Welcome to the products page")
+//   try {
+//     if (text) {
+//       let products = await ProductsModel.find({
+//         title: { $regex: text, $options: "i" },
+//       })
+//         .limit(limit)
+//         .skip(limit * (page - 1));
+//       res.send(products);
+//     }else {
+//       let products = await ProductsModel.find()
+//         .limit(limit)
+//         .skip(limit * (page - 1));
+//       res.send(products);
+//     }
+//   } catch (err) {
+//     res.send(err.message);
+//   }
+// });
+
+productsRoute.get("/", async (req, res) => {
+  const text = req.query.text;
+  const { limit = 20, page = 1, sort, order, category } = req.query;
+
+  try {
+    let query = {};
+    if (text) {
+      query.title = { $regex: text, $options: "i" };
     }
-})
+    if (category) {
+      //   query.category = { $regex: category, $options: "i" };
+      const categories = Array.isArray(category) ? category : [category];
+      query.category = { $in: categories };
+    }
 
-module.exports = productsRoute
+    let productsQuery = ProductsModel.find(query);
+
+    // Sorting based on price
+    if (sort === "price") {
+      let sortOption = {};
+      sortOption.price = order === "desc" ? -1 : 1;
+      productsQuery.sort(sortOption);
+    }
+
+    // Count total number of products matching the query
+    const totalCount = await ProductsModel.countDocuments(query);
+    // Pagination
+    const totalPages = Math.ceil(totalCount / limit);
+    const currentPage = parseInt(page);
+    productsQuery.limit(limit).skip(limit * (currentPage - 1));
+
+    let products = await productsQuery.exec();
+
+    res.send({
+      totalItems: totalCount,
+      totalPages: totalPages,
+      currentPage: currentPage,
+      items: products,
+    });
+  } catch (err) {
+    res.send(err.message);
+  }
+});
+
+module.exports = productsRoute;
