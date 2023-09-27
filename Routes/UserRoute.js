8e617a51c888a2cdf303039f7cde9dd3491676b2
Ayush Verma <ayush98277@gmail.com>
2023-09-27 21:27:27 +0530

    sending userDetails while log in

--- Routes/UserRoute.js
+++ Routes/UserRoute.js
@@ -12,11 +12,11 @@ userRouter.post("/signup", async (req, res) => {
   const regExp = /[a-zA-Z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{3,8}/g;
   const userData = await UserModel.find({ email });
 
-  console.log("userData",userData);
+  console.log("userData", userData);
 
   try {
     if (userData.length > 0) {
-      return res.send({message : "User already exists"});
+      return res.send({ message: "User already exists" });
     } else {
       try {
         bcrypt.hash(password, 5, async (err, hashPass) => {
@@ -39,31 +39,39 @@ userRouter.post("/signup", async (req, res) => {
   }
 });
 
-userRouter.post("/login", async(req,res)=>{
-  const {email, password} = req.body;
+userRouter.post("/login", async (req, res) => {
+  const { email, password } = req.body;
 
   console.log(email, password);
 
   try {
-    const user = await UserModel.find({email});
-    if(user.length>0){
-      bcrypt.compare(password, user[0].password, (err,result)=>{
-        if(result){
-          const token = jwt.sign({userID: user[0]._id}, process.env.key, {expiresIn:"24h"})
-          return res.status(201).send({ message: "Login Successfull", token: token });
-        }else{
+    const user = await UserModel.find({ email });
+    if (user.length > 0) {
+      bcrypt.compare(password, user[0].password, (err, result) => {
+        if (result) {
+          const token = jwt.sign({ userID: user[0]._id }, process.env.key, {
+            expiresIn: "24h",
+          });
+          return res
+            .status(201)
+            .send({
+              message: "Login Successfull",
+              token: token,
+              userDetails: user,
+            });
+        } else {
           return res.status(403).send({ message: "Wrong password" });
         }
-      } )
-    }else{
-      return res.status(403).send({ message: "User does not exist with this email" });
+      });
+    } else {
+      return res
+        .status(403)
+        .send({ message: "User does not exist with this email" });
     }
-
   } catch (err) {
-    console.log("err",err)
+    console.log("err", err);
     return res.status(403).send({ message: "Wrong email or password" });
   }
+});
 
-})
-
-module.exports = userRouter
+module.exports = userRouter;
